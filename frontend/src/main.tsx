dbf2b86cb820b24ee775fc0b6c60abfc1f7298a7
jingkang97 <ngjingkang97@gmail.com>
2026-09-04 20:31:59 +0800

    feat: add pitch-rail QR, lock phones to portrait, and reset onboarding scroll

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { LanguageProvider } from './i18n/LanguageContext.tsx'
+import { lockPhonePortrait } from './lib/lockPhonePortrait'
+
+lockPhonePortrait()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
